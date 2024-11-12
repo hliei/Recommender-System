bbe5302e53a5697b32266c6d3dd4c2b5079caf96
hliei <hliei@connect.ust.hk>
2024-11-12 09:41:39 +0800

    new model

--- model.py
+++ model.py
@@ -15,13 +15,14 @@ class PositiveProp(nn.Module):
             nn.Linear(embedding_dim, embedding_dim),
             nn.ReLU(),
             nn.Linear(embedding_dim, embedding_dim),
-            nn.ReLu()
+            nn.ReLU()
         )
         self.attention1 = nn.Linear(embedding_dim, embedding_dim)
         self.attention2 = nn.Linear(embedding_dim, 2)
 
     def forward(self, edge_index, edge_label_index):
         z_prime = self.lightgcn(edge_index, edge_label_index)
+        z_prime = self.lightgcn.get_embedding(edge_index)
         z_double_prime = self.mlp(self.lightgcn.embedding.weight)
 
         attention_score_1 = F.tanh(self.attention1(z_prime))
@@ -32,11 +33,38 @@ class PositiveProp(nn.Module):
         attention_score = torch.cat([attention_score_1, attention_score_2], dim=1)
         attention_score = F.softmax(attention_score, dim=1)
 
-        alpha_1 = attention_score[:, 0].unsqueeze(1).expand_as(self.lightgcn.embedding.weight)
-        alpha_2 = attention_score[:, 1].unsqueeze(1).expand_as(self.lightgcn.embedding.weight)
+        alpha_1 = attention_score[:, 0].unsqueeze(1).expand_as(z_prime)
+        alpha_2 = attention_score[:, 1].unsqueeze(1).expand_as(z_double_prime)
         
-        z = alpha_1 * z_prime + alpha_2 * z_double_prime
+        z_emb = alpha_1 * z_prime + alpha_2 * z_double_prime
+
+        out_src = z_emb[edge_label_index[0]]
+        out_dst = z_emb[edge_label_index[1]]
+        z = (out_src * out_dst).sum(dim=-1)
         return z
+    
+    def loss_fn(self, z, edge_label_index):
+        z_i, z_j = z.chunk(2)
+        return self.lightgcn.recommendation_loss(z_i, z_j, node_id=edge_label_index.unique())
+    
+    def get_embedding(self,edge_index):
+        z_prime = self.lightgcn.get_embedding(edge_index)
+        z_double_prime = self.mlp(self.lightgcn.embedding.weight)
+
+        attention_score_1 = F.tanh(self.attention1(z_prime))
+        attention_score_1 = self.attention2(attention_score_1)
+        attention_score_2 = F.tanh(self.attention1(z_double_prime))
+        attention_score_2 = self.attention2(attention_score_2)
+
+        attention_score = torch.cat([attention_score_1, attention_score_2], dim=1)
+        attention_score = F.softmax(attention_score, dim=1)
+
+        alpha_1 = attention_score[:, 0].unsqueeze(1).expand_as(z_prime)
+        alpha_2 = attention_score[:, 1].unsqueeze(1).expand_as(z_double_prime)
+        
+        z_emb = alpha_1 * z_prime + alpha_2 * z_double_prime
+
+        return z_emb
     
     
 class NegativeProp(nn.Module):
@@ -47,3 +75,31 @@ class NegativeProp(nn.Module):
     def forward(self, edge_index, edge_label_index):
         v = self.lightgcn(edge_index, edge_label_index)
         return v
+    
+    def loss_fn(self, z, edge_label_index):
+        z_i, z_j = z.chunk(2)
+        return self.lightgcn.recommendation_loss(z_i, z_j,node_id = edge_label_index.unique())
+    
+
+
+class FinalModel(nn.Module):
+    def __init__(self, num_users, num_items, embedding_dim, num_layers=2):
+        super(FinalModel, self).__init__()
+        self.num_users = num_users
+        self.num_items = num_items
+        self.num_nodes = num_users + num_items
+        self.positive_prop = PositiveProp(self.num_nodes, embedding_dim, num_layers)
+        self.negative_prop = NegativeProp(self.num_nodes, embedding_dim, num_layers)
+
+    def forward(self, pos_edge_index, neg_edge_index, pos_edge_label_index, neg_edge_label_index):
+
+        z = self.positive_prop(pos_edge_index, pos_edge_label_index)
+        v = self.negative_prop(neg_edge_index, neg_edge_label_index)
+
+        return z, v 
+    
+    def bpr_loss(self,z,v,pos_edge_label_index,neg_edge_label_index):
+        return self.positive_prop.loss_fn(z,pos_edge_label_index), self.negative_prop.loss_fn(v,neg_edge_label_index)
+    
+    def get_2_embedding(self,pos_edge_index, neg_edge_index):
+        return self.positive_prop.get_embedding(pos_edge_index),self.negative_prop.lightgcn.get_embedding(neg_edge_index)
--- train_eval.py
+++ train_eval.py
@@ -1,13 +1,198 @@
-import os.path as osp
-
 import torch
 from tqdm import tqdm
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 from torch_geometric.datasets import MovieLens100K
 from torch_geometric.nn import LightGCN
 from torch_geometric.utils import degree
 
+from model import FinalModel
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+def load_movielens_100k(path):
+    df = pd.read_csv(path, sep='\t', names=['user_id', 'item_id', 'rating', 'timestamp'])
+    df['user_id'] -= 1  
+    df['item_id'] -= 1  
+    return df
+
+def create_edge_data(df):
+    user_item_interactions = df[['user_id', 'item_id']].to_numpy().T
+    user_item_rating = df['rating'].astype(float).to_numpy()
+    edge_index = torch.tensor(user_item_interactions, dtype=torch.long) 
+    edge_weight = torch.tensor(user_item_rating, dtype=torch.float32)
+    pos_edge_index, neg_edge_index = get_2_edge_index(edge_index,edge_weight)
+    return edge_index, edge_weight, pos_edge_index, neg_edge_index
+
+def get_2_edge_index(edge_index,edge_weight):
+    pos_index = edge_weight>=3
+    neg_index = edge_weight<3
+    pos_edge_index = edge_index[:,pos_index]
+    neg_edge_index = edge_index[:,neg_index]
+    return pos_edge_index, neg_edge_index
+
 # Load the dataset
-dataset = MovieLens100K(root='dataset')
-data = dataset[0]
+movielens_path = 'dataset/raw/u.data'
+dataset = load_movielens_100k(movielens_path)
+num_users, num_items = dataset['user_id'].nunique(), dataset['item_id'].nunique()
+train_data, test_data = train_test_split(dataset, test_size=0.2, random_state=42)
+train_edge_index, train_edge_weight, train_pos_edge_index, train_neg_edge_index = create_edge_data(train_data)
+train_edge_index = train_edge_index.to(device)
+
+batch_size = 256
+train_loader = torch.utils.data.DataLoader(
+    range(train_edge_index.size(1)),
+    shuffle=True,
+    batch_size=batch_size,
+)
+
+model = FinalModel(num_users , num_items, embedding_dim=32, num_layers=3).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+def train():
+    total_loss = total_examples = 0
+
+    for index in tqdm(train_loader):
+        batch_edge_index = train_edge_index[:,index]
+        batch_edge_weight = train_edge_weight[index]
+        pos_index = batch_edge_weight>=3
+        neg_index = batch_edge_weight<3
+        pos_edge_index = batch_edge_index[:,pos_index]
+        neg_edge_index = batch_edge_index[:,neg_index]
+        pos_edge_index_j = torch.stack([
+            pos_edge_index[0],
+            torch.randint(num_users, num_users + num_items,
+                          (len(pos_edge_index[0]), ), device=device)
+        ], dim=0)
+        neg_edge_index_j = torch.stack([
+            neg_edge_index[0],
+            torch.randint(num_users, num_users + num_items,
+                          (len(neg_edge_index[0]), ), device=device)
+        ], dim=0)
+        pos_edge_label_index = torch.cat([
+            pos_edge_index,
+            pos_edge_index_j,
+        ], dim=1)
+        neg_edge_label_index = torch.cat([
+            neg_edge_index,
+            neg_edge_index_j,
+        ], dim=1)
+        optimizer.zero_grad()
+        z,v = model(train_pos_edge_index, train_neg_edge_index,pos_edge_label_index, neg_edge_label_index)
+        z_i, z_j = z.chunk(2)
+        v_i, v_j = v.chunk(2)
+        loss1,loss2 = model.bpr_loss(z,v,pos_edge_label_index, neg_edge_label_index)
+        loss = loss1+loss2
+        loss.backward()
+        optimizer.step()
+        total_loss += float(loss1) * z_i.numel() + float(loss2) * v_i.numel()
+        total_examples += z_i.numel() + v_i.numel()
+    return total_loss / total_examples
+
+@torch.no_grad()
+def evaluate(edge_index,pos_edge_index,neg_edge_index,num_users,K=5, thershold=0.5):
+    pos_emb, neg_emb = model.get_2_embedding(pos_edge_index, neg_edge_index)
+    pos_user_emb, pos_item_emb = pos_emb[:num_users], pos_emb[num_users:]
+    neg_user_emb, neg_item_emb = neg_emb[:num_users], neg_emb[num_users:]
+
+    precision = recall = total_examples = 0
+    for start in range(0, num_users, batch_size):
+        end = start + batch_size
+        logits_pos = pos_user_emb[start:end] @ pos_item_emb.t()
+        logits_neg = neg_user_emb[start:end] @ neg_item_emb.t()
+        logits_pos = torch.sigmoid(logits_pos)
+        logits_neg = torch.sigmoid(logits_neg)
+
+        # Exclude examples from training set
+        mask_pos = ((pos_edge_index[0] >= start) &
+                (pos_edge_index[0] < end))
+        logits_pos[pos_edge_index[0, mask_pos] - start,
+               pos_edge_index[1, mask_pos] - num_users] = float('-inf')
+        mask_neg = ((neg_edge_index[0] >= start) &
+                (neg_edge_index[0] < end))
+        logits_neg[neg_edge_index[0, mask_neg] - start,
+                neg_edge_index[1, mask_neg] - num_users] = float('-inf')
+        
+        # Filter out user-item set for logits_neg < thershold
+        mask = logits_neg < thershold 
+        logits_pos_filtered = logits_pos * mask
+
+        # Ground truth
+        ground_truth = torch.zeros_like(logits_pos_filtered, dtype=torch.bool)
+        mask = ((edge_index[0] >= start) &
+                (edge_index[0] < end))
+        ground_truth[edge_index[0, mask] - start,
+                     edge_index[1, mask] - num_users] = True
+        node_count = degree(edge_index[0, mask] - start,
+                            num_nodes=logits_pos_filtered.size(0))
+        
+        # Calculate metrics
+        topk_index = logits_pos_filtered.topk(K, dim=-1).indices
+        isin_mat = ground_truth.gather(1, topk_index)
+        precision += float((isin_mat.sum(dim=-1) / K).sum())
+        recall += float((isin_mat.sum(dim=-1) / node_count.clamp(1e-6)).sum())
+        total_examples += int((node_count > 0).sum())
+
+    return precision / total_examples, recall / total_examples
+
+for epoch in range(1, 3):
+    loss = train()
+    precision, recall = evaluate(train_edge_index, train_pos_edge_index, train_neg_edge_index, num_users, K=15)
+    print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Precision@K: '
+          f'{precision:.4f}, Recall@K: {recall:.4f}')
+
+# Evaluate on the test set
+test_edge_index, test_edge_weight, test_pos_edge_index, test_neg_edge_index = create_edge_data(test_data)
+test_edge_index = test_edge_index.to(device)
+test_pos_edge_index = test_pos_edge_index.to(device)
+test_neg_edge_index = test_neg_edge_index.to(device)
+model.eval()
+@torch.no_grad()
+def evaluate_test(edge_index,pos_edge_index,neg_edge_index,K=5, thershold=0.5):
+    pos_emb, neg_emb = model.get_2_embedding(pos_edge_index, neg_edge_index)
+    pos_user_emb, pos_item_emb = pos_emb[:num_users], pos_emb[num_users:]
+    neg_user_emb, neg_item_emb = neg_emb[:num_users], neg_emb[num_users:]
+    
+    precision = recall = total_examples = 0
+    for start in range(0, num_users, batch_size):
+        end = start + batch_size
+        logits_pos = pos_user_emb[start:end] @ pos_item_emb.t()
+        logits_neg = neg_user_emb[start:end] @ neg_item_emb.t()
+        logits_pos = torch.sigmoid(logits_pos)
+        logits_neg = torch.sigmoid(logits_neg)
+
+        # Exclude examples from training set
+        mask_pos = ((pos_edge_index[0] >= start) &
+                (pos_edge_index[0] < end))
+        logits_pos[pos_edge_index[0, mask_pos] - start,
+               pos_edge_index[1, mask_pos] - num_users] = float('-inf')
+        mask_neg = ((neg_edge_index[0] >= start) &
+                (neg_edge_index[0] < end))
+        logits_neg[neg_edge_index[0, mask_neg] - start,
+                neg_edge_index[1, mask_neg] - num_users] = float('-inf')
+        
+        # Filter out user-item set for logits_neg < thershold
+        mask = logits_neg < thershold 
+        logits_pos_filtered = logits_pos * mask
+
+        # Ground truth
+        ground_truth = torch.zeros_like(logits_pos_filtered, dtype=torch.bool)
+        mask = ((edge_index[0] >= start) &
+                (edge_index[0] < end))
+        ground_truth[edge_index[0, mask] - start,
+                     edge_index[1, mask] - num_users] = True
+        node_count = degree(edge_index[0, mask] - start,
+                            num_nodes=logits_pos_filtered.size(0))
+        
+        # Calculate metrics
+        topk_index = logits_pos_filtered.topk(K, dim=-1).indices
+        isin_mat = ground_truth.gather(1, topk_index)
+        precision += float((isin_mat.sum(dim=-1) / K).sum())
+        recall += float((isin_mat.sum(dim=-1) / node_count.clamp(1e-6)).sum())
+        total_examples += int((node_count > 0).sum())
+
+    return precision / total_examples, recall / total_examples
+
+precision, recall = evaluate_test(test_edge_index, test_pos_edge_index, test_neg_edge_index, K=15)
+print(f'Test Precision@K: {precision:.4f}, Test Recall@K: {recall:.4f}')
+
